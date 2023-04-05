52c454a4bf8677f847faa27e1cffb418ac5e09e0
Caio <caiocezartg@gmail.com>
2023-04-05 16:25:00 -0300

    chore: create env config setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app";
+import { env } from "./env/config";
+
+app.listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+}).then((port) => {
+    console.log('Server is running! ' + port);
+})
