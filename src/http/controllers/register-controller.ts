4994d6f420d1434b288a748a5f14d5ad34fda30f
Caio <caiocezartg@gmail.com>
2023-04-05 23:35:49 -0300

    feat: add register repository and services

---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { registerService } from '../services/register-service';
+
+export async function registerController (request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+  });
+  
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    await registerService({name, email, password});
+  } catch (error) {
+    return reply.status(409).send();
+  }
+  
+  return reply.status(201).send();
+  
+}
